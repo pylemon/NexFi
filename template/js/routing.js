f3ca7f82720cc42db219e96d22741fc498876ac1
leeway <liwei@bolo.me>
2016-09-15 17:39:37 +0800

    前端json解析，绘制拓扑结构图

--- template/js/routing.js
+++ template/js/routing.js
@@ -1,54 +1,127 @@
 var data = {
-  "source_version" : "2013.3.0-14-gcd34783",
+  "source_version" : "2014.4.0",
   "algorithm" : 4,
   "vis" : [
-    { "primary" : "fe:f0:00:00:04:01",
+    { "primary" : "0e:03:01:00:10:14",
       "neighbors" : [
-         { "router" : "fe:f0:00:00:04:01",
-           "neighbor" : "fe:f0:00:00:05:01",
-           "metric" : "1.000" },
-         { "router" : "fe:f0:00:00:04:01",
-           "neighbor" : "fe:f0:00:00:03:01",
-           "metric" : "1.008" }
+         { "router" : "0e:03:01:00:10:14",
+           "neighbor" : "0e:03:01:00:10:0a",
+           "metric" : "1.016" },
+         { "router" : "0e:03:01:00:10:14",
+           "neighbor" : "0e:03:01:00:10:1d",
+           "metric" : "1.032" }
       ],
       "clients" : [
-         "00:00:43:05:00:04",
-         "fe:f1:00:00:04:01"
+        "9a:10:83:ec:19:2b",
+        "33:33:00:00:00:02",
+        "01:00:5e:00:00:01",
+        "33:33:ff:00:00:00",
+        "33:33:00:01:00:02",
+        "33:33:00:01:00:03",
+        "3e:03:01:00:10:14",
+        "33:33:ff:00:10:14",
+        "9a:10:83:ec:19:2b",
+        "33:33:00:00:00:01"
       ]
     },
-    { "primary" : "fe:f0:00:00:02:01",
+    { "primary" : "0e:03:01:00:10:0a",
       "neighbors" : [
-         { "router" : "fe:f0:00:00:02:01",
-           "neighbor" : "fe:f0:00:00:03:01",
-  	       "metric" : "1.000" },
-         { "router" : "fe:f0:00:00:02:01",
-           "neighbor" : "fe:f0:00:00:01:01",
-           "metric" : "1.016" },
-         { "router" : "fe:f0:00:00:02:01",
-           "neighbor" : "fe:f0:00:00:08:01",
-           "metric" : "1.000" }
+         { "router" : "0e:03:01:00:10:0a",
+           "neighbor" : "0e:03:01:00:10:14",
+           "metric" : "1.028" },
+         { "router" : "0e:03:01:00:10:0a",
+           "neighbor" : "0e:03:01:00:10:1d",
+           "metric" : "1.032" }
       ],
       "clients" : [
-        "fe:f1:00:00:02:01",
-        "00:00:43:05:00:02"
+        "33:33:00:00:00:02",
+        "3a:ea:b0:6a:9e:e7",
+        "33:33:ff:00:10:0a",
+        "01:00:5e:00:00:01",
+        "33:33:ff:00:00:00",
+        "3e:03:01:00:10:0a",
+        "33:33:00:01:00:02",
+        "33:33:00:01:00:03",
+        "3a:ea:b0:6a:9e:e7",
+        "33:33:00:00:00:01"
+      ]
+    },
+    { "primary" : "0e:03:01:00:10:1d",
+      "neighbors" : [
+         { "router" : "0e:03:01:00:10:1d",
+           "neighbor" : "0e:03:01:00:10:0a",
+           "metric" : "1.000" },
+         { "router" : "0e:03:01:00:10:1d",
+           "neighbor" : "0e:03:01:00:10:14",
+           "metric" : "1.028" }
+      ],
+      "clients" : [
+        "33:33:00:00:00:02",
+        "01:00:5e:00:00:01",
+        "33:33:ff:00:00:00",
+        "33:33:ff:00:10:1d",
+        "33:33:00:01:00:02",
+        "33:33:00:01:00:03",
+        "06:8e:64:eb:a1:d6",
+        "06:8e:64:eb:a1:d6",
+        "3e:03:01:00:10:1d",
+        "33:33:00:00:00:01"
       ]
     }
   ]
 }
 
+//var data = {
+//  "vis": [
+//    {
+//      "primary" : "0e:03:01:00:10:14",
+//      "neighbors" : [
+//        { "router" : "0e:03:01:00:10:14",
+//          "neighbor" : "0e:03:01:00:10:0a",
+//          "metric" : "1.028" }
+//      ]
+//    },
+//    {
+//      "primary" : "0e:03:01:00:10:0a",
+//      "neighbors" : []
+//    }
+//  ]
+//};
+
+var WIDTH = 1000;
+var HEIGHT = 600;
+
+var positionGenerator = function () {
+    var POSITIONS = [
+       [50, 50], [200, 50], [350, 50], [500, 50], [650, 50], [800, 50],
+       [50, 150], [200, 150], [350, 150], [500, 150], [650, 150], [800, 150],
+       [50, 250], [200, 250], [350, 250], [500, 250], [650, 250], [800, 250],
+       [50, 350], [200, 350], [350, 350], [500, 350], [650, 350], [800, 350],
+       [50, 450], [200, 450], [350, 450], [500, 450], [650, 450], [800, 450],
+       [50, 550], [200, 550], [350, 550], [500, 550], [650, 550], [800, 550]
+    ];
+    POSITIONS = POSITIONS.reverse();
+    return function() {
+        var pos = POSITIONS.pop();
+        if (pos != undefined) {
+            return {x: pos[0], y: pos[1]};
+        } else {
+            return {x: 0, y: 0};
+        }
+    }
+};
+
 
 var graph = new joint.dia.Graph();
-
 var paper = new joint.dia.Paper({
     el: $('#paper'),
-    width: 1000,
-    height: 600,
+    width: WIDTH,
+    height: HEIGHT,
     gridSize: 10,
     model: graph
 });
 
 var allNodes = [];
-
 var newNode = function (name, px, py) {
     // 已存在的节点
     for (i = 0; i < allNodes.length; i ++ ) {
@@ -90,12 +163,39 @@ var newRoute = function(node1, node2, label) {
     var link = new joint.dia.Link({
         source: { id: node1.id },
         target: { id: node2.id },
-        router: { name: 'manhattan' },
-        connector: { name: 'rounded' },
+        router: function(vertices, args, linkView) {
+            if (linkView.sourcePoint && linkView.targetPoint) {
+                var x1 = linkView.sourcePoint.x;
+                var y1 = linkView.sourcePoint.y;
+                var x2 = linkView.targetPoint.x;
+                var y2 = linkView.targetPoint.y;
+                var middlePoint = {
+                    x: ((x1 + x2) / 2),
+                    y: ((y1 + y2) / 2)
+                }
+                var offset = 8;
+                if (x1 > x2 && y1 < y2) {
+                    middlePoint.x += offset;
+                    middlePoint.y += offset;
+                } else if (x1 > x2 && y1 > y2) {
+                    middlePoint.x -= offset;
+                    middlePoint.y += offset;
+                } else if (x1 < x2 && y1 < y2) {
+                    middlePoint.x += offset;
+                    middlePoint.y -= offset;
+                } else if (x1 <= x2 && y1 >= y2) {
+                    middlePoint.x -= offset;
+                    middlePoint.y -= offset;
+                }
+                vertices.push(middlePoint)
+            }
+            return vertices
+        },
+        connector: { name: 'smooth' },
         attrs: {
             '.connection': {
                 stroke: '#333333',
-                'stroke-width': 2
+                'stroke-width': 1
             },
             '.marker-target': {
                 fill: '#333333',
@@ -104,7 +204,7 @@ var newRoute = function(node1, node2, label) {
         }
     });
     link.label(0, {
-        position: 0.5,
+        position: 0.6,
         attrs: {
             text: { fill: '#333333', text: label }
         }
@@ -115,19 +215,36 @@ var newRoute = function(node1, node2, label) {
 }
 
 
-var p1 = newNode("fe:f0:00:00:04:01", 50, 50)
-var p2 = newNode("fe:f0:00:00:02:01", 200, 50)
-var p1_n1 = newNode("fe:f0:00:00:05:01", 350, 50)
-var p1_n2 = newNode("fe:f0:00:00:03:01", 500, 50)
-var p2_n1 = newNode("fe:f0:00:00:03:01", 50, 150)
-var p2_n2 = newNode("fe:f0:00:00:01:01", 200, 150)
-var p2_n3 = newNode("fe:f0:00:00:08:01", 350, 150)
+// 解析原始JSON数据，返回全部需要绘制的节点列表
+var getNodesFromJson = function (jsonData) {
+    var getPos = positionGenerator();
+    nodes = {};
 
-var p1_r1 = newRoute(p1, p1_n1, "1.000")
-var p1_r2 = newRoute(p1, p1_n2, "1.008")
-var p2_r1 = newRoute(p2, p2_n1, "1.000")
-var p2_r2 = newRoute(p2, p2_n2, "1.016")
-var p2_r3 = newRoute(p2, p2_n3, "1.000")
+    // 绘制 primary 节点 和 neighbors 节点
+    for (i = 0; i < data.vis.length; i++) {
+        var visDt = data.vis[i];
+        var pos = getPos();
+        var node = newNode(visDt.primary, pos.x, pos.y);
+        nodes[visDt.primary] = node;
+        for (k = 0; k < visDt.neighbors.length; k++) {
+            var neighbor = visDt.neighbors[k];
+            var pos = getPos();
+            var node = newNode(neighbor.neighbor, pos.x, pos.y);
+            nodes[neighbor.neighbor] = node;
+        }
+    }
+
+//    console.log(nodes)
+
+    // 绘制路由
+    for (i = 0; i < data.vis.length; i++) {
+        var visDt = data.vis[i];
+        for (k = 0; k < visDt.neighbors.length; k++) {
+            var neighbor = visDt.neighbors[k];
+            newRoute(nodes[neighbor.router], nodes[neighbor.neighbor], neighbor.metric);
+        }
+    }
+}()
 
 
 graph.on('change:position', function(cell) {
