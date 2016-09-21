403088afa5fb9f2301eae58f8a7bbd6cb8d0f0bf
leeway <liwei@bolo.me>
2016-09-21 23:04:31 +0800

    加入topo的模板

--- src/main/router.go
+++ src/main/router.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 )
 
 func topoHandler(w http.ResponseWriter, r *http.Request) {
 	logger.Println("topoHandler")
+	t, err := template.ParseFiles("template/html/topo.html")
+	if (err != nil) {
+		logger.Println(err)
+	}
+	t.Execute(w, nil)
 }
 
 
