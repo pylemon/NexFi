28983e9d990321891992bca4119bbba75a816f54
leeway <liwei@bolo.me>
2016-09-24 23:12:38 +0800

    add system cmd

--- src/main/router.go
+++ src/main/router.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"runtime/debug"
 )
 
 func topoHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Println("topoHandler")
+	logger.Println("GET /topo/ topoHandler")
 	t, err := template.ParseFiles("template/html/topo.html")
 	if err != nil {
 		logger.Println(err)
@@ -14,9 +17,53 @@ func topoHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+// 获取拓扑结构的JSON数据
+func topoVisHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Println("GET /topo/vis topoVisHandler")
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Println(err)
+			debug.PrintStack()
+		}
+	}()
+
+	var out bytes.Buffer
+	System("ls", &out)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out.Bytes())
+}
+
+// 一个ajax请求的例子
+type BaseResponse struct {
+	Result int    `json:"result"`
+	Data   string `json:"data"`
+}
+
+func exampleAjaxHandler(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Println(err)
+			debug.PrintStack()
+		}
+	}()
+
+	var baseResp BaseResponse
+	buf, err := json.Marshal(baseResp)
+	if err != nil {
+		http.Error(w, "Marshal JSON failed", 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf)
+
+}
+
 func init() {
 	http.Handle("/css/", http.FileServer(http.Dir("template")))
 	http.Handle("/js/", http.FileServer(http.Dir("template")))
 
 	http.HandleFunc("/topo/", topoHandler)
+	http.HandleFunc("/topo/vis", topoVisHandler)
+	http.HandleFunc("/example", exampleAjaxHandler)
 }
--- src/main/utils.go
+++ src/main/utils.go
@@ -1,8 +1,22 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"log"
+	"os"
+	"os/exec"
+)
 
 func Exist(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil || os.IsExist(err)
+}
+
+func System(s string, output *bytes.Buffer) {
+	cmd := exec.Command("/bin/sh", "-c", s)
+	cmd.Stdout = output
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
