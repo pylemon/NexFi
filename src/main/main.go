a63bf0baede5afbc8656d7daca877674f8f89566
leeway <liwei@bolo.me>
2016-10-04 16:08:46 +0800

    make project in one go file for easier complie

--- src/main/main.go
+++ src/main/main.go
@@ -13,6 +13,14 @@ import (
 	"sync"
 	"syscall"
 	"time"
+	"strings"
+	"encoding/base64"
+	"bytes"
+	"encoding/gob"
+	"path"
+	"errors"
+	"os/exec"
+	"html/template"
 )
 
 // 全局配置结构
@@ -70,7 +78,7 @@ func init() {
 		*VisCommand = "batadv-vis -f jsondoc"
 	}
 
-	if !Exist(*ConfigFile) {
+	if !FileExists(*ConfigFile) {
 		Config.LogFile = *LogFile
 		Config.ListenAddress = *ListenAddress
 		Config.LogToStdout = *LogToStdout
@@ -151,6 +159,7 @@ func main() {
 
 	go signalCallback()
 
+	initRouter()
 	s := &http.Server{
 		Addr:           Config.ListenAddress,
 		Handler:        nil,
@@ -159,5 +168,351 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	logger.Printf("Server [PID: %d] listen on [%s]\n", os.Getpid(), Config.ListenAddress)
-	logger.Fatal(s.ListenAndServe())
+	logger.Println(s.ListenAndServe())
+
+}
+
+func initRouter() {
+	// 路由初始化
+	auth := NewBasicAuth(Config.Username, Config.Password)
+	http.Handle("/css/", http.FileServer(http.Dir("template")))
+	http.Handle("/js/", http.FileServer(http.Dir("template")))
+
+	http.HandleFunc("/", auth.Wrap(homeHandler))
+	http.HandleFunc("/topo/vis", auth.Wrap(topoVisHandler))
+	http.HandleFunc("/topo/position", auth.Wrap(topoPositionHandler))
+}
+
+// 登陆验证 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+type BasicAuth struct {
+	username string
+	password string
+}
+
+type BasicHandlerFunc func(http.ResponseWriter, *http.Request)
+
+func (a *BasicAuth) CheckAuth(r *http.Request) bool {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 {
+		return false
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return false
+	}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false
+	}
+
+	return pair[0] == a.username && pair[1] == a.password
+}
+
+func (a *BasicAuth) Wrap(wrapped BasicHandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if ok := a.CheckAuth(r); ok {
+			wrapped(w, r)
+		} else {
+			logger.Println("auth failed!")
+			w.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
+			w.WriteHeader(401)
+			w.Write([]byte("401 Unauthorized\n"))
+		}
+	}
+}
+
+func NewBasicAuth(username, password string) *BasicAuth {
+	return &BasicAuth{username, password}
+}
+
+// 路由模块 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 渲染首页模板
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Println("GET / homeHandler")
+	t, err := template.ParseFiles("template/html/topo.html")
+	if err != nil {
+		logger.Println(err)
+	}
+	t.Execute(w, nil)
+}
+
+// 获取拓扑结构的JSON数据
+func topoVisHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Println("GET /topo/vis topoVisHandler")
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Println(err)
+			debug.PrintStack()
+		}
+	}()
+
+	var out bytes.Buffer
+	//logger.Println("[vis] cmd:", Config.VisCommand)
+	System(Config.VisCommand, &out)
+	//logger.Println("[vis] response:", out.String())
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out.Bytes())
+}
+
+type Position struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+type Node struct {
+	Name     string   `json:"name"`
+	Position Position `json:"position"`
+}
+
+type VisPosition struct {
+	Nodes []Node `json:"nodes"`
+}
+
+type Response struct {
+	Status string `json:"status"`
+}
+
+func getDatabase(name string) *DB {
+	db := NewDB(
+		"ts",
+		func() interface{} {
+			var vis VisPosition
+			return &vis
+		})
+	db.Init()
+	return db
+}
+
+func topoPositionHandler(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Println(err)
+			debug.PrintStack()
+		}
+	}()
+
+	if r.Method == "POST" {
+		logger.Println("POST /topo/position topoPositionHandler")
+		var dt VisPosition
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&dt)
+		if err != nil {
+			logger.Println(err)
+		}
+		db := getDatabase("position")
+		db.Update("position", dt)
+
+		resp := Response{"ok"}
+		buf, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(w, "Marshal JSON failed", 500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf)
+	} else {
+		logger.Println("GET /topo/position topoPositionHandler")
+		db := getDatabase("position")
+		value := db.Get("position")
+
+		logger.Println("value:", value)
+
+		buf, err := json.Marshal(value)
+		if err != nil {
+			http.Error(w, "Marshal JSON failed", 500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf)
+	}
+}
+
+// 本地文件数据库 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+var g_debug = false
+
+func debugOutput(args ...interface{}) {
+	if g_debug {
+		logger.Println(args)
+	}
+}
+
+var default_db_root_path = "./gobdb"
+
+func NewDB(name string, objPrototypeGenerator func() interface{}) *DB {
+	if len(name) <= 0 {
+		name = "demo"
+	}
+
+	location := path.Join(default_db_root_path, name)
+	if objPrototypeGenerator == nil {
+		objPrototypeGenerator = func() interface{} {
+			var s string
+			return &s
+		}
+	}
+	db := &DB{
+		location:           location,
+		Name:               name,
+		ObjectsMap:         make(map[string]interface{}),
+		prototypeGenerator: objPrototypeGenerator,
+	}
+	return db
+}
+
+type DB struct {
+	location           string
+	Name               string
+	ObjectsMap         map[string]interface{}
+	prototypeGenerator func() interface{}
+}
+
+func (db *DB) Init() (*DB, error) {
+	debugOutput("db location => ", db.location)
+	if FileExists(db.location) == false || FileIsDir(db.location) == false {
+		debugOutput("location not exists: ", db.location)
+		if err := os.MkdirAll(db.location, os.ModePerm); err != nil {
+			return nil, err
+		}
+	}
+
+	if files, err := ListDirFiles(db.location); err != nil {
+		return nil, err
+	} else {
+		debugOutput("files: ", files)
+		StringMap(func(file string) {
+			filePath := path.Join(db.location, file)
+			obj := db.prototypeGenerator()
+			if err := readFile(filePath, obj); err != nil {
+				return
+			}
+			debugOutput("gobdb => load file ", file, " ", obj)
+			db.ObjectsMap[file] = obj
+
+		}, files)
+	}
+	return db, nil
+}
+
+func readFile(path string, obj interface{}) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	dec := gob.NewDecoder(file)
+	err2 := dec.Decode(obj)
+
+	if err2 != nil {
+		return err2
+	}
+	return nil
+}
+
+func (db *DB) Update(key string, value interface{}) error {
+	err := db.Delete(key)
+	if err != nil {
+		return err
+	}
+	return db.Put(key, value)
+}
+
+func (db *DB) Put(key string, value interface{}) error {
+	filePath := path.Join(db.location, key)
+	if FileExists(filePath) == true {
+		return errors.New("file already exists")
+	}
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	enc := gob.NewEncoder(file)
+	err = enc.Encode(value)
+	if err != nil {
+		return err
+	}
+	db.ObjectsMap[key] = value
+	return nil
+}
+
+func (db *DB) Get(key string) interface{} {
+	if _, ok := db.ObjectsMap[key]; ok {
+		return db.ObjectsMap[key]
+	} else {
+		return nil
+	}
+}
+
+func (db *DB) Has(key string) bool {
+	_, ok := db.ObjectsMap[key]
+	return ok
+}
+
+func (db *DB) Delete(key string) error {
+	if db.Has(key) == true {
+		dbFilePath := path.Join(db.location, key)
+		if FileExists(dbFilePath) == false {
+			return nil
+		}
+		if err := os.Remove(dbFilePath); err != nil {
+			return err
+		}
+		delete(db.ObjectsMap, key)
+	}
+	return nil
+}
+
+func (db *DB) Count() int {
+	return len(db.ObjectsMap)
+}
+
+func (db *DB) DB_FileExists(name string) bool {
+	filePath := path.Join(db.location, name)
+	return FileExists(filePath)
+}
+
+// 工具函数 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func FileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+func FileIsDir(dirname string) bool {
+	info, err := os.Stat(dirname)
+	return err == nil && info.IsDir()
+}
+
+func ListDirFiles(dir string) ([]string, error) {
+	f, err := os.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	fileInfos, err := f.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]string, 0, len(fileInfos))
+	for i := range fileInfos {
+		if !fileInfos[i].IsDir() {
+			result = append(result, fileInfos[i].Name())
+		}
+	}
+	return result, nil
+}
+
+func StringMap(f func(string), data []string) {
+	size := len(data)
+	for i := 0; i < size; i++ {
+		f(data[i])
+	}
+}
+
+func System(s string, output *bytes.Buffer) {
+	cmd := exec.Command("/bin/sh", "-c", s)
+	cmd.Stdout = output
+	err := cmd.Run()
+	if err != nil {
+		logger.Println("[System] command failed:", err)
+	}
 }
